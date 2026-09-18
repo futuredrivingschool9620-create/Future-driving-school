719eb9208f7b7a1a56647f17089e739220d95c85
futuredrivingschool9620 <futuredrivingschool9620@gmail.com>
2026-09-18 19:32:42 +0530

    fix(env): set default WHATSAPP_TEMPLATE_NAME to future_driving_school

--- server/src/config/env.ts
+++ server/src/config/env.ts
@@ -78,7 +78,7 @@ const envSchema = z.object({
   WHATSAPP_API_URL: z.string().default(''),
   WHATSAPP_API_TOKEN: z.string().default(''),
   WHATSAPP_PHONE_NUMBER_ID: z.string().default(''),
-  WHATSAPP_TEMPLATE_NAME: z.string().default('renewal_reminder'),
+  WHATSAPP_TEMPLATE_NAME: z.string().default('future_driving_school'),
 
   // SMS Gateway
   SMS_ENABLED: z
